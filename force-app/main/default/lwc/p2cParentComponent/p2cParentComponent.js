746779d42ea24e7f6d0688b0d2f082117f672eeb
lbergrj <lindem@gmx.com>
2022-02-14 18:05:15 -0300

    P2C Communication Basic Data

---
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class P2cParentComponent extends LightningElement {
+    message = "Message from the parent";
+    cardHeading = "Parent to Child Communication !";
+}
